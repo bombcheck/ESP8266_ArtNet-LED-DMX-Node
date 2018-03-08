8a4e29aa7b4ced54ad8f3e0880075a878b4677d0
Nils Bernhardt <nils.bernhardt@gmx.net>
2018-03-08 11:06:15 +0100

    Updated version tag

--- espArtLeDNode/espArtLeDNode.ino
+++ espArtLeDNode/espArtLeDNode.ino
@@ -32,7 +32,7 @@ extern "C" {
   extern struct rst_info resetInfo;
 }
 
-#define FIRMWARE_VERSION "v1.0.3-dev20"
+#define FIRMWARE_VERSION "v1.0.3"
 #define ART_FIRM_VERSION 0x0200   // Firmware given over Artnet (2 bytes)
 
 
